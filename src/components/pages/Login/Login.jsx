326b21234458a96e3b714ebf0182c7085490e702
suvon1 <jsb234967@gmail.com>
2025-11-02 23:26:54 +0600

    get value from LogIn and Register form filde

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import { Link } from "react-router";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
   return (
     <div className="flex justify-center mt-20">
       <div className="card bg-base-100 w-full max-w-lg">
-        <div className="card-body">
+        <form onSubmit={handleLogin} className="card-body">
           <h2 className="font-extrabold text-5xl text-primary text-center ">
             Log In
           </h2>
           <fieldset className="fieldset mt-5 space-y-4">
             <input
+              name="email"
               type="email"
               className="input bg-base-300 w-full font-black p-5"
               placeholder="Email"
+              required
             />
             <input
+              name="password"
               type="password"
               className="input bg-base-300 w-full font-black p-5"
               placeholder="Password"
+              required
             />
             <div>
               <a className="link link-hover">Forgot password?</a>
@@ -31,7 +41,7 @@ const Login = () => {
               </Link>
             </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/components/pages/Register/Register.jsx
+++ src/components/pages/Register/Register.jsx
@@ -2,37 +2,53 @@ import React from "react";
 import { Link } from "react-router";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const photo = form.photo.value;
+    const password = form.password.value;
+
+  };
   return (
     <div className="flex justify-center mt-20">
       <div className="card bg-base-100 w-full max-w-lg">
-        <div className="card-body">
+        <form onSubmit={handleRegister} className="card-body">
           <h2 className="font-extrabold text-5xl text-primary text-center ">
             Register your account
           </h2>
           <fieldset className="fieldset mt-5 space-y-4">
             {/* Name */}
             <input
+              name="name"
               type="text"
               className="input bg-base-300 w-full font-black p-5"
               placeholder="Name"
             />
             {/* Email */}
             <input
+              name="email"
               type="email"
               className="input bg-base-300 w-full font-black p-5"
               placeholder="Email"
+              required
             />
             {/* Photo-URL */}
             <input
+              name="photo"
               type="text"
               className="input bg-base-300 w-full font-black p-5"
               placeholder="Photo-URL"
+              required
             />
             {/* Password */}
             <input
+              name="password"
               type="password"
               className="input bg-base-300 w-full font-black p-5"
               placeholder="Password"
+              required
             />
 
             <button className="btn btn-primary font-black">Register</button>
@@ -43,7 +59,7 @@ const Register = () => {
               </Link>
             </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
